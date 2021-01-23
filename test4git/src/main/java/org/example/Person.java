81023f7859372ad718db26c5361078a2e035d079
ckg <490175316@qq.com>
2021-01-23 23:06:38 +0800

    15

--- test4git/src/main/java/org/example/Person.java
+++ test4git/src/main/java/org/example/Person.java
@@ -5,4 +5,27 @@ public class Person {
     private String address;
     private Integer age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
